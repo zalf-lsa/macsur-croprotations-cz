44827de6983d220cad0a656506c10a60d604af27
TStella <tommaso.stella@zalf.de>
2017-03-31 14:58:54 +0200

    dev dump

--- step_B/run-work-consumer.py
+++ step_B/run-work-consumer.py
@@ -21,9 +21,11 @@ import gc
 import csv
 import types
 import os
-from datetime import datetime
+from datetime import datetime, date, timedelta
 import dateutil.parser
 from collections import defaultdict, OrderedDict
+import re
+import calendar
 
 import zmq
 
@@ -33,7 +35,7 @@ LOCAL_RUN = True
 
 def create_output(custom_id, result):
 
-    #conversion methods
+    #conversion/support methods
     def convert_date(mydate):
         def DateTimeFromISO(isodate):
             d = dateutil.parser.parse(isodate)
@@ -42,7 +44,10 @@ def create_output(custom_id, result):
         return format(mydate, '%Y%j')
     
     def SWC_to_mm(swc_m3, pwp_m3, thickness_m):
-        return max((swc_m3 - pwp_m3), 0) * thickness_m * 1000
+        if swc_m3 != "n.a.":
+            return max((swc_m3 - pwp_m3), 0) * thickness_m * 1000
+        else: #stage not reached
+            return "n.a."
     
     def soilN_to_kgha(orgN_kg_m3, NO3_kg_m3, NH4_kg_m3, thickness_m):
         return (orgN_kg_m3 + NO3_kg_m3 + NH4_kg_m3) * thickness_m * 10000
@@ -56,10 +61,46 @@ def create_output(custom_id, result):
             "wheat/winter wheat": "WW"
         }[crop]
 
-    out = []
+    def calc_DryD1(DryDcycle, DryD2):
+        if DryD2 != "n.a.":
+            return DryDcycle - DryD2
+        else: #anthesis not reached
+            return "n.a."
+
+    def consolidate_DryD2(DryD2, matur):
+        if DryD2 == "n.a." or matur == "n.a.":
+            return "n.a." #anthesis or maturity not reached
+        else:
+            return DryD2
+    
+    def hydro_year_data(origSpec):
+        is_hydro_year_data = False
+        add_to_year = 0        
+        if re.search('xxxx-10-01', origSpec):
+            is_hydro_year_data = True
+            add_to_year = 1
+        elif re.search('xxx-09-30', origSpec):
+            is_hydro_year_data = True
+        return is_hydro_year_data, add_to_year
+
+    def avg_SWC(cum_SWCY1, cum_SWCY2, year):
+        timespan = 365
+        if calendar.isleap(year):
+            timespan = 366
+        if cum_SWCY1 == "n.a.":#1961
+            timespan = (datetime(1961, 9, 30) - datetime(1961, 1, 1)).days + 1
+            cum_SWCY1 = 0
+        return (cum_SWCY1 + cum_SWCY2)/timespan
+            
+
+    crop_out = []
+    hydroyear_out = []
+
     if len(result.get("data", [])) > 0 and len(result["data"][0].get("results", [])) > 0:
 
-        year_to_vals = defaultdict(dict)
+        crop_year_values = defaultdict(lambda: defaultdict(dict))
+        hydroyear_values = defaultdict(dict)
+        crop_sequence =[]
 
         for data in result.get("data", []):
             results = data.get("results", [])
@@ -69,10 +110,15 @@ def create_output(custom_id, result):
             if len(results) == 0:
                 continue
 
-            assert len(oids) == len(results)
+            assert len(oids) == len(results)            
+
+            hydro_year, add_to_year = hydro_year_data(data["origSpec"])
 
             for kkk in range(0, len(results[0])):
                 vals = {}
+                
+                if str(data["origSpec"]) == '"crop"': #reference order of the crop rotation
+                    crop_sequence.append((results[0][kkk], results[1][kkk]))
 
                 for iii in range(0, len(oids)):
                     oid = oids[iii]
@@ -89,51 +135,76 @@ def create_output(custom_id, result):
                 if "Year" not in vals:
                     print "Missing Year in result section. Skipping results section."
                     continue
-
-                year_to_vals[vals.get("Year", 0)].update(vals)
+                
+                if hydro_year:
+                    hydroyear_values[vals.get("Year") + add_to_year].update(vals)
+                else:
+                    crop_year_values[vals.get("Crop")][vals.get("Year")].update(vals)
         
-        for year, vals in OrderedDict(sorted(year_to_vals.items())).iteritems():
-            if len(vals) > 0:
+        for element in crop_sequence:
+            vals = crop_year_values[element[0]][element[1]]
+
+            crop_out.append([
+                convert_date(vals.get("sowing")),
+                vals.get("anthesis", "n.a."),
+                vals.get("matur", "n.a."),
+                convert_date(vals.get("harv")),
+                crop_id(vals.get("Crop")),
+                vals.get("yield"),
+                vals.get("biomass"),
+                vals.get("roots"),
+                vals.get("LAImax"),
+                vals.get("Nfertil"),
+                vals.get("irrig"),
+                vals.get("N-uptake"),
+                vals.get("Nagb"),
+                vals.get("ETcG"),
+                vals.get("ETaG"),
+                vals.get("TraG"),
+                vals.get("PerG"),
+                SWC_to_mm(vals.get("SWCS1"), vals.get("Pwp1"), 0.3),
+                SWC_to_mm(vals.get("SWCS2"), vals.get("Pwp2"), 1.5),
+                SWC_to_mm(vals.get("SWCA1", "n.a."), vals.get("Pwp1"), 0.3),
+                SWC_to_mm(vals.get("SWCA2", "n.a."), vals.get("Pwp2"), 1.5),
+                SWC_to_mm(vals.get("SWCM1", "n.a."), vals.get("Pwp1"), 0.3),
+                SWC_to_mm(vals.get("SWCM2", "n.a."), vals.get("Pwp2"), 1.5),
+                soilN_to_kgha(vals.get("OrgN1_kgm3"), vals.get("NO31_kgm3"), vals.get("NH41_kgm3"), 0.3),
+                soilN_to_kgha(vals.get("OrgN2_kgm3"), vals.get("NO32_kgm3"), vals.get("NH42_kgm3"), 1.5),
+                soilN_to_kgha(0, vals.get("NO31_kgm3"), vals.get("NH41_kgm3"), 0.3),
+                soilN_to_kgha(0, vals.get("NO32_kgm3"), vals.get("NH42_kgm3"), 1.5),
+                vals.get("NleaG"),
+                vals.get("TRRel"),
+                vals.get("Reduk"),
+                calc_DryD1(vals.get("DryDcycle"), vals.get("DryD2", "n.a.")),
+                consolidate_DryD2(vals.get("DryD2", "n.a."), vals.get("matur", "n.a.")),
+                vals.get("Nresid"),
+            ])
         
-                out.append([
-                    convert_date(vals.get("sowing", "n.a.")),
-                    vals.get("anthesis", "n.a."),
-                    vals.get("matur", "n.a."),
-                    convert_date(vals.get("harv", "n.a.")),
-                    crop_id(vals.get("Crop", "n.a.")),
-                    vals.get("yield")[0],
-                    vals.get("biomass")[0],
-                    vals.get("roots")[0],
-                    vals.get("LAImax")[0],
-                    vals.get("Nfertil", "n.a.")[0],
-                    vals.get("irrig")[0],
-                    vals.get("N-uptake")[0],
-                    vals.get("Nagb")[0],
-                    vals.get("ETcG")[0],
-                    vals.get("ETaG")[0],
-                    vals.get("TraG")[0],
-                    vals.get("PerG")[0][0],
-                    SWC_to_mm(vals.get("SWCS1")[0], vals.get("Pwp1")[0], 0.3),
-                    SWC_to_mm(vals.get("SWCS2")[0], vals.get("Pwp2")[0], 1.5),
-                    SWC_to_mm(vals.get("SWCA1")[0], vals.get("Pwp1")[0], 0.3),
-                    SWC_to_mm(vals.get("SWCA2")[0], vals.get("Pwp2")[0], 1.5),
-                    SWC_to_mm(vals.get("SWCM1")[0], vals.get("Pwp1")[0], 0.3),
-                    SWC_to_mm(vals.get("SWCM2")[0], vals.get("Pwp2")[0], 1.5),
-                    soilN_to_kgha(vals.get("OrgN1_kgm3")[0], vals.get("NO31_kgm3")[0], vals.get("NH41_kgm3")[0], 0.3),
-                    soilN_to_kgha(vals.get("OrgN2_kgm3")[0], vals.get("NO32_kgm3")[0], vals.get("NH42_kgm3")[0], 1.5),
-                    soilN_to_kgha(0, vals.get("NO31_kgm3")[0], vals.get("NH41_kgm3")[0], 0.3),
-                    soilN_to_kgha(0, vals.get("NO32_kgm3")[0], vals.get("NH42_kgm3")[0], 1.5),
-                    vals.get("NleaG")[0],
-                    vals.get("TRRel")[0],
-                    vals.get("Reduk")[0],
-                    vals.get("DryD1", "n.a.")[0],
-                    vals.get("DryD2", "n.a.")[0],
-                    vals.get("Nresid")[0],
-                ])
+        for year in OrderedDict(sorted(hydroyear_values.items())):
+            if year == 2081: #don't need it
+                continue
 
-    return out
+            vals = hydroyear_values[year] 
+            
+            SWC1Y_m3 = avg_SWC(vals.get("SWC1Y1", "n.a."), vals.get("SWC1Y2"), year)
+            SWC2Y_m3 = avg_SWC(vals.get("SWC2Y1", "n.a."), vals.get("SWC2Y2"), year)
 
-#+"Stage,HeatRed,RelDev,"\
+            hydroyear_out.append([
+                year,
+                vals.get("ETaY1", 0) + vals.get("ETaY2"), #in 1961 Y1 is missing
+                vals.get("TraY1", 0) + vals.get("TraY2"),
+                vals.get("PerY1", 0) + vals.get("PerY2"),
+                vals.get("PerY1", 0) + vals.get("PerY2"),
+                SWC_to_mm(SWC1Y_m3, vals.get("Pwp1"), 0.3),
+                SWC_to_mm(SWC2Y_m3, vals.get("Pwp2"), 1.5),
+                
+
+
+            ])
+
+    return crop_out
+
+
 HEADER = \
     "sowing," \
     "anthesis," \
